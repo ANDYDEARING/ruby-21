d4b76769297ef7b65b7c72e2ccf5f4c137a60788
Andy Dearing <andy.dearing@gmail.com>
2019-08-21 08:20:47 -0600

    working deck of cards

--- main.rb
+++ main.rb
@@ -19,6 +19,26 @@ cards
 shuffle()
 deal(int)
 =end
+class Deck
+    def initialize()
+        @number_of_cards = 52
+        @suits = ["SPADES", "DIAMONDS", "HEARTS", "CLUBS"]
+        @values = ["ACE", "2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING"]
+        @cards = []
+        for suit in @suits do
+            for value in @values do
+                @cards.push(Card.new(value,suit))
+            end
+        end
+    end
+    def to_s()
+        deck_string = ""
+        for card in @cards do
+            deck_string += card.to_s + "\n"
+        end
+        return deck_string
+    end
+end
 
 #make a game object
 =begin
@@ -32,6 +52,6 @@ endGame()
 
 #start the game
 #play again?
-card1 = Card.new("ACE", "SPADES")
-puts card1.to_s()
+myDeck = Deck.new
+puts myDeck.to_s
 
